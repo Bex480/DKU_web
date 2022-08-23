13b81feb90fe6c05159c96ce1f682138f1e80612
SheCho123 <111002081+SheCho456@users.noreply.github.com>
2022-08-24 01:41:14 +0200

    Basic routing done
    Instalirao React Router, napravio Navigation Page i implemento basic routing izmedu glavnog screena(App) i dva druga screena(Kontakt i Registar)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Kontakt from './screens/kontaktScreen/kontakt.js'
 import Registar from './screens/registerScreen/registar.js'
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
-        <Kontakt></Kontakt>
+      <h1>Navigacija</h1>
+      <section className='navigationButtons'>
+        <Link to="/kontakt">
+          <button className='contactButton'>Kontakt</button>  
+        </Link>
+        <Link to="/registar">
+          <button className='registerButton'>Registar</button>  
+        </Link>
+      </section>
     </div>
   );
 }
