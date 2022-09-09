69e38b173c1ab69021d5b4f413bf95bee000cf3d
SheCho123 <111002081+SheCho456@users.noreply.github.com>
2022-09-09 16:05:43 +0200

    Build test 4

--- DKU/DKU/DKU_react/src/App.js
+++ DKU/DKU/DKU_react/src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import Contact from './screens/contactScreen/contact'
-import Registar from './screens/registerScreen/register.js'
-import ProjectList from './screens/projectListScreen/projectList.js';
 import { Link, Outlet } from "react-router-dom";
 
 function App() {
