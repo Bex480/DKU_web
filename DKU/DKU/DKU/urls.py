689b8068b5670626b849226a1040bd4cf503485d
Bex480 <bekir.h480@gmail.com>
2022-09-23 16:52:03 +0200

    temp register

--- DKU/DKU/DKU/urls.py
+++ DKU/DKU/DKU/urls.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from DKU.register.views import Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +10,6 @@ urlpatterns = [
     path('kontakt', TemplateView.as_view(template_name='index.html')),
     path('projekti', TemplateView.as_view(template_name='index.html')),
     path('api/', include('api.urls')),
-
-    path('register', Register)
+    path('register/', include('register.urls')),
 ]
 
