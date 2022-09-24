48a77aabc1ffe09be51eb6849afa5328612e8fd8
Bex480 <bekir.h480@gmail.com>
2022-09-24 15:45:25 +0200

    tweaks

--- DKU/DKU/register/views.py
+++ DKU/DKU/register/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
-from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView
 from .serializers import RegisterSerializer
 
