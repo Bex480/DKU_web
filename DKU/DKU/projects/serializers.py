e10ba727739771f1ebdc321250ed1c2329522c1d
Bex480 <bekir.h480@gmail.com>
2022-09-28 15:11:22 +0200

    projects and changes

--- DKU/DKU/projects/serializers.py
+++ DKU/DKU/projects/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Project, ProjectCategory
-
+from django.contrib.auth.models import User
 
 class ProjectSerializer(serializers.ModelSerializer):
 
@@ -20,6 +20,14 @@ class ProjectSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
+
+        leader = User(id=validated_data.pop('leader_id'))
+        supervisor = User(id=validated_data.pop('supervisor_id'))
+        leader.save()
+        supervisor.save()
+
         project = Project(**validated_data)
+        project.leader = leader
+        project.supervisor = supervisor
         project.save()
         return project
