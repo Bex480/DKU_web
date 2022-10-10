94a18431d050624789d8565904c6be0686ed8ef3
Bex480 <bekir.h480@gmail.com>
2022-10-10 17:16:51 +0200

    listing supervisors

--- DKU/DKU/profile/urls.py
+++ DKU/DKU/profile/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GetUser.as_view()),
-    path('/supervisors/', views.GetSupervisors.as_view()),
+    path('supervisors/', views.GetSupervisors.as_view()),
 ]
